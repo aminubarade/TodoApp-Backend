fc38222273ed525db192e7a07b27819438bcde74
aminubarade <aminubaradepage@gmail.com>
2023-02-22 15:51:07 +0100

    initial commit for  sequelize

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -6,8 +6,8 @@ const controllers = require("../../controllers/TaskController");
 router.route("/").get(controllers.getAllTasks);
 router.route("/").post(controllers.createTask);
 router.route("/:id").get(controllers.getTask);
-router.route("/:id").get(controllers.updateTask);
-router.route("/:id").get(controllers.deleteTask);
+router.route("/:id").put(controllers.updateTask);
+router.route("/:id").delete(controllers.deleteTask);
 
 
 
